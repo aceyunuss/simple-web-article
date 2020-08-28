b61c3e625ba13bf1a08373462c1054d862bf8844
Ana <aceyunus@gmail.com>
2020-08-28 17:01:35 +0700

    add max widht image article preview

--- application/views/articles_v.php
+++ application/views/articles_v.php
@@ -1,8 +1,8 @@
 <div class="row">
   <?php foreach ($article as $key => $value) { ?>
-    <div class="col-sm-6 col-md-3">
+    <div class="col-sm-4">
       <div class="thumbnail">
-        <img class="img-rounded" src="<?php echo base_url('/uploads/articles/').$value['image'] ?>" style="max-height: 500px" >
+        <img class="img-rounded" src="<?php echo base_url('/uploads/articles/').$value['image'] ?>" style="max-height: 500px; max-width: 600px" >
         <div class="caption text-center">
           <h3><?php echo $value['title'] ?></h3>
           <p><?php echo substr(strip_tags($value['content']), 0, 110). " ..." ?></p>
